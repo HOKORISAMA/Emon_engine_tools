6d346c91b9146cd437e8c6e549f6fe98771b4313
Hokori <160050298+HOKORISAMA@users.noreply.github.com>
2025-10-23 09:32:30 +0530

    Update EXBMP.cs

--- EmonTool/EXBMP.cs
+++ EmonTool/EXBMP.cs
@@ -1,37 +1,38 @@
-﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using System.Buffers.Binary;
-using Utility.Compression;
+using Compression;
 
 namespace EmonTool
 {
-    internal class EXBMP
+    internal class ExBmp
     {
-        private const int HEADER_SIZE = 32;
-        public static Image DecodeImage(byte[] data, Utils.Entry entry, byte[] key)
+        private const int HeaderSize = 32;
+        public static Image DecodeImage(byte[] data, Entry entry, byte[] key)
         {
             try
             {
-                if (data.Length < HEADER_SIZE)
+                if (data.Length < HeaderSize)
                     throw new InvalidDataException("Failed to decode header");
 
-                var header = data[..HEADER_SIZE].ToArray();
-                Utils.Decrypt(header, 0, HEADER_SIZE, key);
+                var header = data[..HeaderSize].ToArray();
+                Utils.Decrypt(header, 0, HeaderSize, key);
 
-                ushort bpp = (ushort)(BinaryPrimitives.ReadUInt16LittleEndian(header.AsSpan(0)) & 0xFF);
-                int width = BinaryPrimitives.ReadUInt16LittleEndian(header.AsSpan(2));
-                int height = BinaryPrimitives.ReadUInt16LittleEndian(header.AsSpan(4));
-                int colors = BinaryPrimitives.ReadUInt16LittleEndian(header.AsSpan(6));
-                int stride = BinaryPrimitives.ReadInt32LittleEndian(header.AsSpan(8));
-                int offsetX = BinaryPrimitives.ReadInt32LittleEndian(header.AsSpan(0xC));
-                int offsetY = BinaryPrimitives.ReadInt32LittleEndian(header.AsSpan(0x10));
+                EmeMetaData metaData = new EmeMetaData();
+                metaData.Bpp = (ushort)BitConverter.ToUInt16(header, 0) & 0xFF;
+                metaData.Width = BitConverter.ToUInt16(header, 2);
+                metaData.Height = BitConverter.ToUInt16(header, 4);
+                metaData.Colors = BitConverter.ToUInt16(header, 6);
+                metaData.Stride = BitConverter.ToUInt16(header, 8);
+                metaData.OffsetX = (int)BitConverter.ToUInt32(header, 0xC);
+                metaData.OffsetY = (int)BitConverter.ToUInt32(header, 0x10);
 
-                int dataOffset = HEADER_SIZE;
+                int dataOffset = HeaderSize;
                 List<(byte R, byte G, byte B)>? palette = null;
 
-                if (colors > 0 && (data[HEADER_SIZE] & 0xFF) != 7)
+                if (metaData.Colors > 0 && (data[HeaderSize] & 0xFF) != 7)
                 {
-                    int actualColors = Math.Max(colors, 3);
+                    int actualColors = Math.Max(metaData.Colors, 3);
                     palette = ReadPalette(data, dataOffset, actualColors);
                     dataOffset += actualColors * 4;
                 }
@@ -39,31 +40,31 @@ namespace EmonTool
                 byte[] pixelData;
                 if (entry.IsPacked)
                 {
-                    byte[] compressedData = new byte[entry.PackedSize - (dataOffset - HEADER_SIZE)];
+                    byte[] compressedData = new byte[entry.PackedSize - (dataOffset - HeaderSize)];
                     Array.Copy(data, dataOffset, compressedData, 0, compressedData.Length);
                     pixelData = Lzss.Decompress(compressedData);
                 }
                 else
                 {
-                    pixelData = new byte[stride * height];
+                    pixelData = new byte[metaData.Stride * metaData.Height];
                     int copySize = Math.Min(data.Length - dataOffset, pixelData.Length);
                     Array.Copy(data, dataOffset, pixelData, 0, copySize);
                 }
 
                 Image image;
 
-                if (bpp == 7)
+                if (metaData.Bpp == 7)
                 {
-                    image = Image.LoadPixelData<L8>(pixelData, width, height);
+                    image = Image.LoadPixelData<L8>(pixelData, metaData.Width, metaData.Height);
                 }
-                else if (bpp == 32)
+                else if (metaData.Bpp == 32)
                 {
-                    var rgbaImage = new Image<Rgba32>(width, height);
-                    for (int y = 0; y < height; y++)
+                    var rgbaImage = new Image<Rgba32>(metaData.Width, metaData.Height);
+                    for (int y = 0; y <  metaData.Height; y++)
                     {
-                        int srcY = height - 1 - y;
-                        int rowOffset = srcY * stride;
-                        for (int x = 0; x < width; x++)
+                        int srcY =  metaData.Height - 1 - y;
+                        int rowOffset = srcY * metaData.Stride;
+                        for (int x = 0; x < metaData.Width; x++)
                         {
                             int pixelOffset = rowOffset + (x * 4);
                             if (pixelOffset + 4 <= pixelData.Length)
@@ -79,17 +80,17 @@ namespace EmonTool
                     }
                     image = rgbaImage;
                 }
-                else if (bpp == 24)
+                else if (metaData.Bpp == 24)
                 {
-                    var rgbImage = new Image<Rgb24>(width, height);
-                    int rowPadding = stride - (width * 3);
+                    var rgbImage = new Image<Rgb24>(metaData.Width, metaData.Height);
+                    int rowPadding = metaData.Stride - (metaData.Width * 3);
 
-                    for (int y = 0; y < height; y++)
+                    for (int y = 0; y < metaData.Height; y++)
                     {
-                        int srcY = height - 1 - y;  // Flip vertically
-                        int rowOffset = srcY * stride;
+                        int srcY = metaData.Height - 1 - y;  // Flip vertically
+                        int rowOffset = srcY * metaData.Stride;
 
-                        for (int x = 0; x < width; x++)
+                        for (int x = 0; x < metaData.Width; x++)
                         {
                             int pixelOffset = rowOffset + (x * 3);
                             if (pixelOffset + 3 <= pixelData.Length)
@@ -106,12 +107,12 @@ namespace EmonTool
                 }
                 else if (palette != null)
                 {
-                    var rgbImage = new Image<Rgb24>(width, height);
-                    for (int y = 0; y < height; y++)
+                    var rgbImage = new Image<Rgb24>(metaData.Width, metaData.Height);
+                    for (int y = 0; y < metaData.Height; y++)
                     {
-                        int srcY = height - 1 - y;
-                        int rowOffset = srcY * stride;
-                        for (int x = 0; x < width; x++)
+                        int srcY = metaData.Height - 1 - y;
+                        int rowOffset = srcY * metaData.Stride;
+                        for (int x = 0; x < metaData.Width; x++)
                         {
                             int pixelOffset = rowOffset + x;
                             if (pixelOffset < pixelData.Length)
